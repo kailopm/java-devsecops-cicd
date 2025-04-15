718b4f26ae25a0de0c49fa0f36f1e7e407351953
Kailop meepho <007349@g-able.com>
2025-04-15 22:07:25 +0700

    update

--- terraform/scripts/ubnutu/install-jenkins-agent.sh
+++ terraform/scripts/ubnutu/install-jenkins-agent.sh
@@ -55,3 +55,10 @@ sudo apt-get install git -y
 
 # Install Maven (build tool for Java)
 sudo apt-get install maven -y
+
+# Install Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
